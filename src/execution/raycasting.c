77dd9744176a6631b335ffbf0b85ae2d034b56f9
Stephane Kabangu <stkabang@fu-r3-p4.s19.be>
2025-12-09 19:43:15 +0100

    raycasting done?

--- src/execution/raycasting.c
+++ src/execution/raycasting.c
@@ -6,11 +6,20 @@
 /*   By: stkabang <stkabang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 16:25:10 by stkabang          #+#    #+#             */
-/*   Updated: 2025/12/09 19:21:48 by stkabang         ###   ########.fr       */
+/*   Updated: 2025/12/09 19:43:07 by stkabang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cubd3.h"
+
+static double	ft_floor(double nb)
+{
+	if (nb >= 0)
+		return ((double)(int)nb);
+	if (nb != ((int)nb))
+		return ((double)(int)nb - 1);
+	return ((double)(int)nb);
+}
 
 static double	ft_abs_double(double nb)
 {
